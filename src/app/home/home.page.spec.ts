b4aa637335dfa969a701e3d6edbba5a678b861a6
Nikituh <aare.undo@gmail.com>
2021-04-15 17:12:43 +0200

    cleanuP

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,17 +2,13 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePage } from './home.page';
-import {StatusBar} from '@ionic-native/status-bar/ngx';
-import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {RouteReuseStrategy} from '@angular/router';
-import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {IonicRouteStrategy} from '@ionic/angular';
 import {DialogsService} from '../services/dialogs.service';
 import {ScanbotSdkDemoService} from '../services/scanbot-sdk-demo.service';
 import {ImageResultsRepository} from '../services/image-results.repository';
-import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from '../app-routing.module';
 import {IonicStorageModule} from '@ionic/storage';
-import { Storage } from '@ionic/storage';
 
 describe('HomePage', () => {
     let component: HomePage;
